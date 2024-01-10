605dc8ab0d53e50feb648ec8b2aab0d556cd8ec5
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-01-10 15:26:14 +0530

    sss

--- pages/menu.js
+++ pages/menu.js
@@ -2,13 +2,88 @@ import PageHeader from "@/components/common/PageHeader";
 import InstaBlock from "@/components/home-components/InstaBlock";
 import Testimonials from "@/components/home-components/Testimonials";
 import ProductGrid from "@/components/common/ProductGrid";
+import Slider from "react-slick";
 export default function ContactUs() {
+  var settings = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    infinite: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 786,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          centerMode: true,
+          arrows: false,
+          variableWidth: true,
+        },
+      },
+    ],
+  };
   return (
     <>
       <div>
-        <PageHeader title="Delicious Menu" />
+        
         <div className="bg5 pd-common MenuListBox">
           <div className="container">
+            <div className="MenuItemTabWrap">
+            <div
+            className="HmT1 text-center hfont"
+            
+          >
+            Discover the Delights of <br />
+            Fine Real Traditional Dining
+          </div>
+          <div
+            className="HmT2 text-center"
+            
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </div>
+              <div className="MenuSlidet1">
+                <Slider {...settings}>
+                  <div>
+                    <div className="MenuTabItem active">All Menus</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Hot Drink</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Cool Drinks</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Shawarma</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Pizza</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Side Dishes</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Daily Dishes</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Shawarma</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Pizza</div>
+                  </div>
+                  <div>
+                    <div className="MenuTabItem">Side Dishes</div>
+                  </div>
+                </Slider>
+              </div>
+            </div>
             <div className="MenuItemsBox">
               <div className="row">
                 <div className="col-md-3">
