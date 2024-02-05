fd0f80c5ed772de98b1b4d788ccc2237cf584563
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-02-05 12:50:22 +0530

    cccc

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -5,7 +5,9 @@ import AboutAMV from "@/components/about-components/aboutamv";
 import OurVision from "@/components/about-components/OurVision";
 import OurMission from "@/components/about-components/OurMission";
 import HomeGallery from "@/components/home-components/HomeGallery";
+import { useState, React, useRef, useEffect } from "react";
 export default function aboutus() {
+  const videoRef = useRef(null);
   return (
     <>
       
@@ -14,6 +16,21 @@ export default function aboutus() {
         <AboutAMV/>
         <HomeGallery/>
         <OurVision/>
+        <div className="VideoSection pd-common bg5n">
+          <div className="container">
+            <video
+              ref={videoRef}
+              src="/mainbanner.mp4"
+              width="100%"
+              autoPlay
+              muted
+              loop
+              playsInline
+              poster="/hero.jpg"
+              className="MainBannerVideo"
+            />
+          </div>
+        </div>
         <HomeGallery/>
         <OurMission/>
         <Testimonials/>
